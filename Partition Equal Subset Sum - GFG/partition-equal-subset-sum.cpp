61d6ef2f55faf7b97b3ddc936563f516166b66e4
Sourabh Chandel <77907942+sourabh59-coder@users.noreply.github.com>
2023-02-10 08:29:10 +0530

    Added solution - LeetHub

--- Partition Equal Subset Sum - GFG/partition-equal-subset-sum.cpp
+++ Partition Equal Subset Sum - GFG/partition-equal-subset-sum.cpp
@@ -9,41 +9,41 @@ using namespace std;
 
 class Solution{
 public:
-    int fun(int v[],int ind,int k,vector<vector<int>> &dp)
-    {
-        if(k==0)
-        {
-            return 1;
-        }
+    // int fun(int v[],int ind,int k,vector<vector<int>> &dp)
+    // {
+    //     if(k==0)
+    //     {
+    //         return 1;
+    //     }
         
-        if(ind==0)
-        {
-            if(v[0]==k)
-            {
-                return 1;
-            }
-            else
-            {
-                return 0;
-            }
-        }
+    //     if(ind==0)
+    //     {
+    //         if(v[0]==k)
+    //         {
+    //             return 1;
+    //         }
+    //         else
+    //         {
+    //             return 0;
+    //         }
+    //     }
         
-        if(dp[ind][k]!=-1)
-        {
-            return dp[ind][k];
-        }
+    //     if(dp[ind][k]!=-1)
+    //     {
+    //         return dp[ind][k];
+    //     }
         
-        int NotTake = fun(v,ind-1,k,dp);
+    //     int NotTake = fun(v,ind-1,k,dp);
         
-        int Take = 0;
+    //     int Take = 0;
         
-        if(k>=v[ind])
-        {
-            Take = fun(v,ind-1,k-v[ind],dp);
-        }
+    //     if(k>=v[ind])
+    //     {
+    //         Take = fun(v,ind-1,k-v[ind],dp);
+    //     }
         
-        return dp[ind][k] = (Take|NotTake);
-    }
+    //     return dp[ind][k] = (Take|NotTake);
+    // }
     int equalPartition(int n, int v[])
     {
         int sum = 0;
@@ -56,8 +56,35 @@ public:
             return 0;
         }
         int val = sum/2;
-        vector<vector<int>> dp(n,vector<int> ((val)+1,-1));
-        return fun(v,n-1,val,dp);
+        vector<vector<int>> dp(n,vector<int> ((val)+1,0));
+        
+        for(int i=0;i<n;i++)
+        {
+            dp[i][0] = 1;
+        }
+        if(v[0]<=val)
+        {
+            dp[0][v[0]] = 1;
+        }
+        
+        for(int i=1;i<n;i++)
+        {
+            for(int j=1;j<=val;j++)
+            {
+                int NotTake = dp[i-1][j];
+                
+                int Take = 0;
+                
+                if(j>=v[i])
+                {
+                    Take = dp[i-1][j-v[i]];
+                }
+                
+                dp[i][j] = NotTake | Take;
+            }
+        }
+        // return fun(v,n-1,val,dp);
+        return dp[n-1][val];
     }
 };
 
