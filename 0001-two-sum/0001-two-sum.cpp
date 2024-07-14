06dc80b06a14101a9fabf172421cacc10689bf62
Sourabh Chandel <77907942+sourabh59-coder@users.noreply.github.com>
2024-07-14 23:49:31 +0530

    Time: 9 ms (68.44%), Space: 14 MB (39.11%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -1,26 +1,32 @@
 class Solution {
 public:
-    vector<int> twoSum(vector<int>& v, int k) {
-        unordered_map<int,int> mp;
-        vector<int> ans;
-        for(int i=0;i<v.size();i++)
+    int BinarySearch(vector<pair<int,int>> &v,int l,int r,int k)
+    {
+        while(l<=r)
         {
-            mp[v[i]] = i;
+            int mid = l + (r-l)/2;
+
+            if(v[mid].first == k)   return v[mid].second;
+            else if(v[mid].first < k)     l = mid + 1;
+            else                    r = mid - 1;
         }
-        
-        for(int i=0;i<v.size();i++)
+        return -1;
+    }
+    vector<int> twoSum(vector<int>& nums, int target) {
+        vector<pair<int,int>> v;
+        int n = nums.size();
+
+        for(int i=0;i<n;i++)    v.push_back({nums[i],i});
+
+        sort(v.begin(),v.end());
+
+        for(int i=0;i<n-1;i++)
         {
-            int kp = k-v[i];
-            if(mp.find(kp)!=mp.end())
-            {
-                if(mp[kp]!=i)
-                {
-                    ans.push_back(i);
-                    ans.push_back(mp[kp]);
-                    break;
-                }
-            }
+            int temp = BinarySearch(v,i+1,n-1,target-v[i].first);
+
+            if(temp!=-1)    return {v[i].second, temp};
         }
-        return ans;
+
+        return {};
     }
 };
