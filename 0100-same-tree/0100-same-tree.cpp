508c6524343cfa3ec67b3a3c49c8b17d0ecad1b2
Sourabh Chandel <77907942+sourabh59-coder@users.noreply.github.com>
2024-07-15 12:23:57 +0530

    Time: 0 ms (100%), Space: 12.2 MB (5.67%) - LeetHub

--- 0100-same-tree/0100-same-tree.cpp
+++ 0100-same-tree/0100-same-tree.cpp
@@ -11,23 +11,17 @@
  */
 class Solution {
 public:
-    bool Helper(TreeNode* p,TreeNode* q)
-    {
-        if(p==NULL && q==NULL)  return true;
-        
-        if(p==NULL && q!=NULL)  return false;
-        
-        else if(p!=NULL && q==NULL) return false;
-        
-        else
-        {
-            bool a = Helper(p->left,q->left);
-            bool b = Helper(p->right,q->right);
-            
-            return (a&&b&&(p->val==q->val));
-        }
-    }
     bool isSameTree(TreeNode* p, TreeNode* q) {
-        return Helper(p,q);
+        if(p==NULL && q==NULL)  return true;
+        if(p==NULL || q==NULL)  return false;
+
+        if(p->val == q->val)
+        {
+            bool l = isSameTree(p->left,q->left);
+            bool r = isSameTree(p->right,q->right);
+
+            return (l && r); 
+        }
+        return false;        
     }
 };
