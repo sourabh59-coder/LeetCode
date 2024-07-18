226119fac56f19925c979c00014677ec8eeb8ef6
Sourabh Chandel <77907942+sourabh59-coder@users.noreply.github.com>
2024-07-18 13:30:19 +0530

    Time: 3 ms (29.51%), Space: 7.7 MB (7.25%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -13,7 +13,17 @@ public:
         return dp[n] = (one+two);
     }
     int climbStairs(int n) {
-        vector<int> dp(n+1,-1);
-        return fun(n,dp);
+        vector<int> dp(n+1,0);
+        dp[0] = 1;
+        for(int i=1;i<=n;i++)
+        {
+            int one  = 0, two = 0;
+            if(i>=1) one = dp[i-1];
+            if(i>=2) two = dp[i-2];
+
+            dp[i] = (one+two);
+        }
+        return dp[n];
+        // return fun(n,dp);
     }
 };
