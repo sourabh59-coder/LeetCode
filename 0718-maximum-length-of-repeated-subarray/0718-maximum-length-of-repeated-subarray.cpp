9627dc63615709a10a637dc3e5f0dda60c4fe5eb
Sourabh Chandel <77907942+sourabh59-coder@users.noreply.github.com>
2024-07-18 22:06:30 +0530

    Time: 88 ms (55.37%), Space: 88.3 MB (11.44%) - LeetHub

--- 0718-maximum-length-of-repeated-subarray/0718-maximum-length-of-repeated-subarray.cpp
+++ 0718-maximum-length-of-repeated-subarray/0718-maximum-length-of-repeated-subarray.cpp
@@ -1,34 +1,21 @@
 class Solution {
 public:
-    int LongestCommomSubstring(vector<int> v1,vector<int> v2)
-    {
-        int n = v1.size();
-        
-        int m = v2.size();
-        
-        vector<vector<int>> dp(n+1, vector<int> (m+1,0));
-        
+    int findLength(vector<int>& s1, vector<int>& s2) {
+        int n = s1.size(), m = s2.size();
+        vector<vector<int>> dp(n+1,vector<int> (m+1,0));
         int ans = 0;
-        
         for(int i=1;i<=n;i++)
         {
             for(int j=1;j<=m;j++)
             {
-                if(v1[i-1]==v2[j-1])
+                if(s1[i-1]==s2[j-1])
                 {
-                    dp[i][j] = 1+dp[i-1][j-1];
+                    dp[i][j] = dp[i-1][j-1]+1;
                     ans = max(ans,dp[i][j]);
                 }
-                else
-                {
-                    dp[i][j] = 0;
-                }
+                else    dp[i][j] = 0;
             }
         }
-        
         return ans;
-    }
-    int findLength(vector<int>& nums1, vector<int>& nums2) {
-        return LongestCommomSubstring(nums1,nums2);
     }
 };
