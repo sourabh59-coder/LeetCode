2b395c876c152a8e1151a5add15a9bc7c80d9260
Sourabh Chandel <77907942+sourabh59-coder@users.noreply.github.com>
2024-07-20 11:29:14 +0530

    Time: 4 ms (83.97%), Space: 16.9 MB (55.98%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.cpp
+++ 0733-flood-fill/0733-flood-fill.cpp
@@ -1,36 +1,37 @@
 class Solution {
 public:
-    void fun(vector<vector<int>> &v,int i,int j,int col,int prev)
+    vector<pair<int,int>> moments = {{1,0},{0,1},{-1,0},{0,-1}};
+    bool check(int i,int j,int n,int m)
     {
+        return (i>=0 && j>=0 &&i<n && j<m);
+    }
+    void dfs(int i,int j,int color,int curr_color,vector<vector<int>> &v)
+    {
+        int  n = v.size();
+        int  m = v[0].size();
+        if(v[i][j]==curr_color)
+        {
+            v[i][j] = color;
+
+            for(auto moment: moments)
+            {
+                int x = i + moment.first;
+                int y = j + moment.second;
+
+                if(check(x,y,n,m) && v[x][y]==curr_color)
+                {
+                    dfs(x,y,color,curr_color,v);
+                }
+            }
+        }
+    }
+    vector<vector<int>> floodFill(vector<vector<int>>& v, int sr, int sc, int color) {
         int n = v.size();
         int m = v[0].size();
-        if(i<0 || i>=n)
-        {
-            return;
-        }
-        if(j<0 || j>=m)
-        {
-            return;
-        }
-        if(v[i][j]!=prev)
-        {
-            return;
-        }
-        
-        v[i][j] = col;
-        fun(v,i+1,j,col,prev);
-        fun(v,i-1,j,col,prev);
-        fun(v,i,j+1,col,prev);
-        fun(v,i,j-1,col,prev);
-    }
-    vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
-        int iniCol = image[sr][sc];
-        
-        if(iniCol!=color)
-        {
-            fun(image,sr,sc,color,image[sr][sc]);
-        }
-        
-        return image;
+        int curr_color = v[sr][sc];
+        if(curr_color==color)   return v;
+
+        dfs(sr,sc,color,curr_color,v);
+        return v;
     }
 };
