17d45b8fa694eec80dd386270c858aae4df9b262
Sourabh Chandel <77907942+sourabh59-coder@users.noreply.github.com>
2024-07-21 12:38:11 +0530

    Time: 7 ms (49.03%), Space: 15.4 MB (5.22%) - LeetHub

--- 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.cpp
+++ 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.cpp
@@ -1,35 +1,40 @@
 class Solution {
 public:
+    void CreateMapNode(Node* head, unordered_map<Node*, Node*>& mp) {
+        Node* temp = head;
+        while (temp != NULL) {
+            Node* ptr = new Node(temp->val);
+            mp[temp] = ptr;
+            temp = temp->next;
+        }
+    }
+
+    Node* createList(Node* head, unordered_map<Node*, Node*>& mp) {
+        Node* ptr = head;
+        while (ptr != NULL) {
+            mp[ptr]->next = mp[ptr->next];
+            ptr = ptr->next;
+        }
+        return mp[head];
+    }
+
+    void createRandom(Node* head, unordered_map<Node*, Node*>& mp) {
+        Node* temp = head;
+        while (temp != NULL) {
+            if (temp->random) {
+                mp[temp]->random = mp[temp->random];
+            }
+            temp = temp->next;
+        }
+    }
+
     Node* copyRandomList(Node* head) {
-        if(!head)
-            return NULL;
-        Node* head_cp = NULL;
-        Node* curr = head;
-        Node* curr_cp = NULL;
-//(I)CONSTRUCTING THE NEW LIST WITH THE CURRENT VALUE AND THE NEXT VALUE WITH RANDOM SET AS NULL
-        while(curr!=NULL){
-            curr_cp = new Node(curr->val,curr->next,NULL);
-            curr->next = curr_cp;
-            curr = curr_cp->next;
-        }
-//(II)SETTING THE PREVIOUS NODE VALUE AT THIS STEP
-        curr = head;
-        while(curr!=NULL){
-            curr_cp = curr->next;
-            if(curr->random)
-                curr_cp->random = curr->random->next;
-            curr = curr_cp->next;
-        }
-//(III) INORDER TO RETURN THE VALUES FROM THE NEW LIST WE NEED TO ARRANGE IT SUCH THAT PROPER ORDER WE CAN RETURN 
-        curr = head;
-        head_cp = head->next;
-        while(curr!=NULL){
-            curr_cp = curr->next;
-            curr->next = curr_cp->next; // making the question's list back to normal
-            curr = curr->next;
-            if(curr)
-                curr_cp->next = curr->next;
-        }
-        return head_cp;
+        if (!head) return nullptr;
+        
+        unordered_map<Node*, Node*> mp;
+        CreateMapNode(head, mp);
+        Node* newHead = createList(head, mp);
+        createRandom(head, mp);
+        return newHead;
     }
 };
