fe73c11f72dcb594ecbb936cf71e578d70b4b9bd
Sourabh Chandel <77907942+sourabh59-coder@users.noreply.github.com>
2024-07-21 18:54:14 +0530

    Time: 122 ms (14.85%), Space: 18.5 MB (12.43%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.cpp
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.cpp
@@ -8,32 +8,51 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
-// 	bool operator( ListNode* a, ListNode* b){
-//         return (a->val>b->val);
-//         }
-	
 class Solution {
-	public:
-	ListNode* mergeKLists(vector<ListNode*>& lists) {
-        auto cmp = [](ListNode* a, ListNode* b) { return a->val > b->val; };
-        priority_queue<ListNode*, vector<ListNode*>, decltype(cmp)> pq(cmp);
-        
-        for (auto list : lists) {
-            if (list) pq.push(list);
+public:
+    ListNode* mergeList(ListNode* l1,ListNode* l2)
+    {
+        ListNode* dummy = new ListNode(-1);
+        ListNode* ptr = dummy;
+        while(l1 && l2)
+        {
+            if(l1->val<l2->val)
+            {
+                ptr->next = l1;
+                l1 = l1->next;
+                ptr = ptr->next;
+            }
+            else
+            {
+                ptr->next = l2;
+                l2 = l2->next;
+                ptr = ptr->next;
+            }
         }
-        
-        ListNode dummy(0);
-        ListNode* curr = &dummy;
-        while (!pq.empty()) {
-            auto node = pq.top();
-            pq.pop();
-            
-            curr->next = node;
-            curr = curr->next;
-            
-            if (node->next) pq.push(node->next);
+
+        if(l1)
+        {
+            ptr->next = l1;
         }
-        
-        return dummy.next;
+
+        if(l2)
+        {
+            ptr->next = l2;
+        }
+
+        return dummy->next;
+    }
+    ListNode* mergeKLists(vector<ListNode*>& lists) {
+        int n = lists.size();
+        if(n==0)    return NULL;
+
+        ListNode* start = lists[0];
+        ListNode* second = NULL;
+        for(int i=1;i<n;i++)
+        {
+            second = lists[i];
+            start = mergeList(start,second);
+        }
+        return start;
     }
 };
