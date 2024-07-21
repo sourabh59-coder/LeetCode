845afda5f9aceb6a6c2e947235202946707d7766
Sourabh Chandel <77907942+sourabh59-coder@users.noreply.github.com>
2024-07-21 16:33:24 +0530

    Time: 5 ms (14.13%), Space: 10.5 MB (10.67%) - LeetHub

--- 0092-reverse-linked-list-ii/0092-reverse-linked-list-ii.cpp
+++ 0092-reverse-linked-list-ii/0092-reverse-linked-list-ii.cpp
@@ -1,23 +1,52 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
 class Solution {
 public:
-    ListNode* reverseBetween(ListNode* head, int left, int right) 
+    ListNode* reverseList(ListNode* head)
     {
-        if (right==1) return head;
-        ListNode dummy=ListNode();
-        ListNode* L=&dummy, *R;
-        dummy.next=head;
-        // Move L to the position left
-        for(int i=0; i<left-1; i++)
-            L=L->next;
-        R=L->next;
-        //From i=left to right, move R->next to the position right stepwise
-        for(int i=left; i<right; i++){
-            ListNode* rev=R->next;//Reverse the linked list (1)
-            R->next=rev->next;//(2)position for R->next in next i 
-            rev->next=L->next;//(3)
-            L->next=rev; //(4)
+        if(head==NULL || head->next==NULL)  return head;
+        ListNode* prev = NULL;
+        ListNode* ptr = head;
+        while(ptr!=NULL)
+        {
+            ListNode* ptr_next = ptr->next;
+            ptr->next = prev;
+            prev = ptr;
+            ptr = ptr_next;
         }
+        return prev;
+    }
+    ListNode* reverseBetween(ListNode* head, int left, int right) {
+        if(head==NULL || head->next==NULL || left==right || right==1)   return head;
 
-        return dummy.next;
+        ListNode* dummy = new ListNode(-510);
+        dummy->next = head;
+        head = dummy;
+        ListNode* prev = NULL;
+        ListNode* slow = head;
+        ListNode* fast = head;
+
+        for(int i=0;i<left;i++)
+        {
+            prev = slow;
+            slow = slow->next;
+        } 
+        for(int i=0;i<right;i++)    fast = fast->next;
+
+        ListNode* fast_next = fast->next;
+        fast->next = NULL;
+        ListNode* temp = reverseList(slow);
+        prev->next = temp;
+        while(temp->next!=NULL) temp = temp->next;
+        temp->next = fast_next;
+        return dummy->next;
     }
 };
