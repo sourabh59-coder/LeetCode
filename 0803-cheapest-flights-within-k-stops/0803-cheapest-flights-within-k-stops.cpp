5f0a84eb37b9963091c18d7fce0f49de9e28cd61
Sourabh Chandel <77907942+sourabh59-coder@users.noreply.github.com>
2024-07-22 19:10:35 +0530

    Time: 21 ms (27.19%), Space: 17.5 MB (20.87%) - LeetHub

---
+++ 0803-cheapest-flights-within-k-stops/0803-cheapest-flights-within-k-stops.cpp
@@ -0,0 +1,45 @@
+class Solution {
+public:
+    const int INF = 1e9;
+    int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int k) {
+        // priority_queue will store {cost, {node, stops}}
+        queue<pair<int, pair<int, int>>> qe;
+        
+        // adj will store {destination, weight}
+        vector<vector<pair<int, int>>> adj(n);
+        for(auto it : flights) {
+            int a = it[0];
+            int b = it[1];
+            int wt = it[2];
+            adj[a].emplace_back(b, wt);
+        }
+
+        vector<int> dist(n, INF);
+        dist[src] = 0;
+        qe.push({0, {src, 0}});
+
+        while(!qe.empty()) {
+            auto it = qe.front();
+            qe.pop();
+
+            int cost = it.first;
+            int node = it.second.first;
+            int stops = it.second.second;
+
+            // if(node == dst) return cost;
+            if(stops==k+1) break;
+
+            for(auto& child : adj[node]) {
+                int child_node = child.first;
+                int child_wt = child.second;
+
+                if(cost + child_wt < dist[child_node]) {
+                    dist[child_node] = cost + child_wt;
+                    qe.push({cost + child_wt, {child_node, stops + 1}});
+                }
+            }
+        }
+
+        return (dist[dst] == INF) ? -1 : dist[dst];
+    }
+};
