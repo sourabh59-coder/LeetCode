74ad8e619ccf33bfb2dca586f511937f41f4e502
Sourabh Chandel <77907942+sourabh59-coder@users.noreply.github.com>
2024-07-23 23:28:10 +0530

    Time: 125 ms (36.67%), Space: 106.7 MB (34.1%) - LeetHub

--- 0077-combinations/0077-combinations.cpp
+++ 0077-combinations/0077-combinations.cpp
@@ -1,8 +1,8 @@
 class Solution {
 public:
-    void fun(int n,int k,int ind,vector<vector<int>> &ans,vector<int> &temp)
+    void fun(int ind,int n,int k,vector<int> &temp,vector<vector<int>> &ans)
     {
-        if(temp.size()==k)
+        if(k==temp.size())
         {
             ans.push_back(temp);
             return;
@@ -10,14 +10,14 @@ public:
         if(ind>n)   return;
         
         temp.push_back(ind);
-        fun(n,k,ind+1,ans,temp);
+        fun(ind+1,n,k,temp,ans);
         temp.pop_back();
-        fun(n,k,ind+1,ans,temp);
+        fun(ind+1,n,k,temp,ans);
     }
     vector<vector<int>> combine(int n, int k) {
         vector<vector<int>> ans;
         vector<int> temp;
-        fun(n,k,1,ans,temp);
+        fun(1,n,k,temp,ans);
         return ans;
     }
 };
