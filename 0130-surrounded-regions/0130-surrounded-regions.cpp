a04ec39556a1e11c97674e4ec0acffdbc1f7ed8f
Sourabh Chandel <77907942+sourabh59-coder@users.noreply.github.com>
2024-07-23 10:18:29 +0530

    Time: 12 ms (38.81%), Space: 14.2 MB (25.13%) - LeetHub

--- 0130-surrounded-regions/0130-surrounded-regions.cpp
+++ 0130-surrounded-regions/0130-surrounded-regions.cpp
@@ -1,77 +1,67 @@
 class Solution {
 public:
-    vector<pair<int,int>> movements{{1,0},{-1,0},{0,1},{0,-1}};
-    void DFS(vector<vector<char>> &v,vector<vector<int>> &vis,int i,int j,int m,int n)
+    vector<pair<int,int>> moments = {{1,0},{0,1},{-1,0},{0,-1}};
+    bool check(int i,int j,int n,int m)
+    {
+        return (i>=0 && j>=0 && i<n && j<m);
+    }
+    void dfs(vector<vector<char>> &v,int i,int j,int n,int m,vector<vector<int>> &vis)
     {
         vis[i][j] = 1;
-        
-        for(auto movement: movements)
+
+        for(auto moment: moments)
         {
-            int child_i = movement.first+i;
-            int child_j = movement.second+j;
-            
-            if(child_i>=0 && child_i<m && child_j>=0 && child_j<n)
+            int x = i + moment.first;
+            int y = j + moment.second;
+
+            if(check(x,y,n,m) && !vis[x][y] && v[x][y]=='O')
             {
-                if(v[child_i][child_j]=='O' && !vis[child_i][child_j])
-                {
-                    DFS(v,vis,child_i,child_j,m,n);
-                }
+                dfs(v,x,y,n,m,vis);
             }
         }
     }
-    void Reg(vector<vector<char>> &v,int i,int j,int m,int n)
+    void dfs_change(vector<vector<char>> &v,int i,int j,int n,int m,vector<vector<int>> &vis)
     {
         v[i][j] = 'X';
-        
-        for(auto movement: movements)
+        vis[i][j] = 1;
+
+        for(auto moment: moments)
         {
-            int child_i = movement.first+i;
-            int child_j = movement.second+j;
-            
-            if(child_i>=0 && child_i<m && child_j>=0 && child_j<n)
+            int x = i + moment.first;
+            int y = j + moment.second;
+
+            if(check(x,y,n,m) && !vis[i][j] && v[i][j]=='O')
             {
-                if(v[child_i][child_j]=='O')
-                {
-                    Reg(v,child_i,child_j,m,n);
-                }
+                dfs_change(v,x,y,n,m,vis);
             }
         }
     }
     void solve(vector<vector<char>>& v) {
-        int m = v.size(), n = v[0].size();
-        vector<vector<int>> vis(m, vector<int> (n,0));
-        
-        for(int j=0;j<n;j++)
+        int n = v.size();
+        int m = v[0].size();
+
+        vector<vector<int>> vis(n, vector<int> (m,0));
+        for(int i=0;i<n;i++)
         {
-            if(v[0][j]=='O')
+            for(int j=0;j<m;j++)
             {
-                DFS(v,vis,0,j,m,n);
-            }
-            if(v[m-1][j]=='O')
-            {
-                DFS(v,vis,m-1,j,m,n);
-            }
-        }
-        
-        for(int i=0;i<m;i++)
-        {
-            if(v[i][0]=='O')
-            {
-                DFS(v,vis,i,0,m,n);
-            }
-            if(v[i][n-1]=='O')
-            {
-                DFS(v,vis,i,n-1,m,n);
-            }
-        }
-        
-        for(int i=0;i<m;i++)
-        {
-            for(int j=0;j<n;j++)
-            {
-                if(v[i][j]=='O' && !vis[i][j])
+                if(i==0 || j==0 || i==n-1 || j==m-1)
                 {
-                    Reg(v,i,j,m,n);
+                    if(v[i][j]=='O' && !vis[i][j])
+                    {
+                        dfs(v,i,j,n,m,vis);
+                    }
+                }
+            }
+        }
+
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<m;j++)
+            {
+                if(!vis[i][j] && v[i][j]=='O')
+                {
+                    dfs_change(v,i,j,n,m,vis);
                 }
             }
         }
