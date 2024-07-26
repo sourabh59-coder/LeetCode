3564d9aaf878b290d573028f7bfb5b658955cfbf
Sourabh Chandel <77907942+sourabh59-coder@users.noreply.github.com>
2024-07-26 10:20:15 +0530

    Time: 6 ms (88.29%), Space: 16.9 MB (12.33%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -3,68 +3,25 @@ public:
     void setZeroes(vector<vector<int>>& v) {
         int n = v.size();
         int m = v[0].size();
-        bool x=false, y=false;
-        for(int i=0;i<n;i++)
-        {
-            if(v[i][0]==0)
-            {
-                x = true;
-            }
-        }
-        for(int j=0;j<m;j++)
-        {
-            if(v[0][j]==0)
-            {
-                y = true;
-            }
-        }
+
+        vector<int> i_cap(n,0);
+        vector<int> j_cap(m,0);
+
         for(int i=0;i<n;i++)
         {
             for(int j=0;j<m;j++)
             {
                 if(v[i][j]==0)
                 {
-                    v[0][j] = 0;
-                    v[i][0] = 0;
+                    i_cap[i] = 1;
+                    j_cap[j] = 1;
                 }
             }
         }
-        
-        for(int i=1;i<n;i++)
-        {
-            if(v[i][0] == 0)
-            {
-                for(int j=0;j<m;j++)
-                {
-                    v[i][j] = 0;
-                }
-            }
-        }
-        
-        for(int j=1;j<m;j++)
-        {
-            if(v[0][j]==0)
-            {
-                for(int i=0;i<n;i++)
-                {
-                    v[i][j] = 0;
-                }
-            }
-        }
-        
-        if(x)
-        {
-            for(int i=0;i<n;i++)
-            {
-                v[i][0] = 0;
-            }
-        }
-        if(y)
-        {
+
+        for(int i=0;i<n;i++)
             for(int j=0;j<m;j++)
-            {
-                v[0][j] = 0;
-            }
-        }
+                if(i_cap[i]==1 || j_cap[j]==1)
+                    v[i][j] = 0;
     }
 };
