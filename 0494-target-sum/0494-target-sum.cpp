fedffc48ae011267394f67c7da1887851ef9d5dd
Sourabh Chandel <77907942+sourabh59-coder@users.noreply.github.com>
2024-09-06 00:02:58 +0530

    Time: 8 ms (65.78%), Space: 18.6 MB (32.58%) - LeetHub

--- 0494-target-sum/0494-target-sum.cpp
+++ 0494-target-sum/0494-target-sum.cpp
@@ -1,21 +1,39 @@
 class Solution {
 public:
-    int fun(vector<int> &v,int ind,int n,int k)
-    {
-        if(ind==n)
-        {
-            if(k==0)    return 1;
-            else        return 0;
-        }
-
-        int notTake = fun(v,ind+1,n,k-v[ind]);
-
-        int Take = fun(v,ind+1,n,k+v[ind]);
-
-        return (notTake + Take);
-    }
-    int findTargetSumWays(vector<int>& v, int k) {
+    int findTargetSumWays(vector<int>& v, int d) {
         int n = v.size();
-        return fun(v,0,n,k);
+        int sum = 0;
+        for(auto it: v) sum += it;
+        
+        vector<vector<int>> dp(n+1, vector<int> (sum+1,0));
+        // fun(v,n,sum,dp);
+        for(int i=0;i<=n;i++)   dp[i][0] = 1;
+        
+        for(int i=1;i<=n;i++)
+        {
+            for(int j=0;j<=sum;j++)
+            {
+                //notTake
+                int notTake = dp[i-1][j];
+                
+                // Take
+                int Take = 0;
+                
+                if(v[i-1]<=j) Take = dp[i-1][j-v[i-1]];
+                
+                dp[i][j] = (Take+notTake);
+            }
+        }
+        int ans = 0;
+        // return dp[n][sum];
+        for(int j=0;j<=sum;j++)
+        {
+            if(dp[n][j]!=0)
+            {
+                if(abs(sum-(2*j))==abs(d) && (sum/2) >= j)  ans += dp[n][j];
+            }
+        }
+        
+        return ans;
     }
 };
