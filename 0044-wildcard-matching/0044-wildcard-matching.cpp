1746c77cb28d1a0aa9d761eddd5c3b8ee4671050
Sourabh Chandel <77907942+sourabh59-coder@users.noreply.github.com>
2024-09-06 15:59:51 +0530

    Time: 38 ms (54.85%), Space: 31.9 MB (10.92%) - LeetHub

--- 0044-wildcard-matching/0044-wildcard-matching.cpp
+++ 0044-wildcard-matching/0044-wildcard-matching.cpp
@@ -1,62 +1,30 @@
 class Solution {
 public:
-    int fun(string &s1,string &s2,int i,int j, vector<vector<int>> &dp)
+    int fun(string &s,string &p,int n,int m,vector<vector<int>> &dp)
     {
-        if(i<0 && j<0)  return true;
-
-        if(i<0 && j>=0)
-        {
-            for(int k =0;k<=j;k++)
-            {
-                if(s2[k]!='*')  return false;
+        if(n==0 && m==0)    return 1;
+        if (m == 0) return 0; 
+        if (n == 0) {
+            for (int i = 0; i < m; i++) {
+                if (p[i] != '*') return 0;
             }
-
-            return true;
+            return 1;
         }
 
-        if(j<0 && i>=0) return false;
+        if(dp[n][m]!=-1)    return dp[n][m];
 
-        if(dp[i][j]!=-1)    return dp[i][j];
+        if(s[n-1]==p[m-1])  return dp[n][m]=  (fun(s,p,n-1,m-1,dp));
 
-        if(s1[i]==s2[j])    return dp[i][j] = fun(s1,s2,i-1,j-1,dp);
+        if(p[m-1]=='*')     return dp[n][m] = (fun(s,p,n-1,m,dp) || fun(s,p,n-1,m-1,dp) || fun(s,p,n,m-1,dp));
 
-        if(s2[j]=='?')  return dp[i][j] = fun(s1,s2,i-1,j-1,dp);
+        if(p[m-1]=='?')     return dp[n][m] = (fun(s,p,n-1,m-1,dp));
 
-        if(s2[j]=='*')  return dp[i][j] = fun(s1,s2,i-1,j,dp) | fun(s1,s2,i-1,j-1,dp) | fun(s1,s2,i,j-1,dp);
-
-        return dp[i][j] = false;
+        return dp[n][m] = 0;
     }
-    bool isMatch(string s1, string s2) {
-        int n = s1.length();
-        int m = s2.length();
-        vector<vector<int>> dp(n+1, vector<int> (m+1,0));
-        // return fun(s1,s2,n-1,m-1,dp);
-        dp[0][0] = 1;
-
-        for(int j=1;j<=m;j++)
-        {
-            bool ans = 1;
-
-            for(int k=1;k<=j;k++)
-            {
-                if(s2[k-1]!='*')    ans = 0;
-            }
-
-            dp[0][j] = ans;
-        }
-
-        for(int i=1;i<=n;i++)
-        {
-            for(int j=1;j<=m;j++)
-            {
-                if(s1[i-1]==s2[j-1])    dp[i][j] = dp[i-1][j-1];
-
-                if(s2[j-1]=='?')  dp[i][j] = dp[i-1][j-1];
-
-                if(s2[j-1]=='*')  dp[i][j] = dp[i-1][j] | dp[i-1][j-1] | dp[i][j-1];
-            }
-        }
-
-        return dp[n][m];
+    bool isMatch(string s, string p) {
+        int n = s.length();
+        int m = p.length();
+        vector<vector<int>> dp(n+1, vector<int> (m+1,-1));
+        return fun(s,p,n,m,dp);
     }
 };
