166b01c7c2bda60b65e16bc03f139511a78bfde9
Sourabh Chandel <77907942+sourabh59-coder@users.noreply.github.com>
2024-09-06 17:24:40 +0530

    Time: 502 ms (22.04%), Space: 295.2 MB (5.89%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -6,17 +6,19 @@ public:
 
         if(dp[ind][prev+1]!=-1) return dp[ind][prev+1];
 
+        // notTake 
         int notTake = 0 + fun(v,ind+1,prev,n,dp);
 
+        //Take
         int Take = 0;
 
-        if(prev==-1 || v[prev]<v[ind]) Take = 1 + fun(v,ind+1,ind,n,dp);
+        if(prev==-1 || v[ind]>v[prev])  Take = fun(v,ind+1,ind,n,dp) + 1;
 
-        return dp[ind][prev+1] = max(Take,notTake);
+        return dp[ind][prev+1] = max(notTake,Take);
     }
     int lengthOfLIS(vector<int>& v) {
         int n = v.size();
-        vector<vector<int>> dp(n+1, vector<int> (n+2,-1));
+        vector<vector<int>> dp(n+1, vector<int> (n+1,-1));
         return fun(v,0,-1,n,dp);
     }
 };
