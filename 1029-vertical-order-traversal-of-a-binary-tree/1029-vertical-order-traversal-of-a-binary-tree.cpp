6e1d806c1b9bca05766349398435cc1720277da2
Sourabh Chandel <77907942+sourabh59-coder@users.noreply.github.com>
2024-09-17 15:20:57 +0530

    Time: 4 ms (55.83%), Space: 14.3 MB (77.69%) - LeetHub

--- 1029-vertical-order-traversal-of-a-binary-tree/1029-vertical-order-traversal-of-a-binary-tree.cpp
+++ 1029-vertical-order-traversal-of-a-binary-tree/1029-vertical-order-traversal-of-a-binary-tree.cpp
@@ -1,35 +1,50 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
 public:
-    void bfs(TreeNode* root, map<int, map<int, multiset<int>>>& mp) {
-        if (root == NULL) return;
-        
-        queue<pair<TreeNode*, pair<int, int>>> q;
-        q.push({root, {0, 0}});
-        
-        while (!q.empty()) {
-            auto [node, pos] = q.front();
-            q.pop();
-            
-            int x = pos.first, y = pos.second;
-            mp[x][y].insert(node->val);
-            
-            if (node->left) q.push({node->left, {x - 1, y + 1}});
-            if (node->right) q.push({node->right, {x + 1, y + 1}});
-        }
-    }
-    
     vector<vector<int>> verticalTraversal(TreeNode* root) {
-        map<int, map<int, multiset<int>>> nodes;
-        bfs(root, nodes);
-        
-        vector<vector<int>> result;
-        for (auto& [x, ys] : nodes) {
-            vector<int> col;
-            for (auto& [y, vals] : ys) {
-                col.insert(col.end(), vals.begin(), vals.end());
-            }
-            result.push_back(col);
+        map<int,map<int,multiset<int>>> mp; // ver, level, data
+        queue<pair<TreeNode*,pair<int,int>>> qe; // level, vertical
+        vector<vector<int>> ans;
+        if(root==NULL)  return ans;
+
+        qe.push({root,{0,0}});
+
+        while(!qe.empty())
+        {
+            auto it = qe.front();
+            qe.pop();
+
+            auto node = it.first;
+            int level = it.second.first;
+            int ver = it.second.second;
+
+            mp[ver][level].insert(node->val);
+
+            if(node->left)  qe.push({node->left,{level+1,ver-1}});
+            if(node->right) qe.push({node->right,{level+1,ver+1}});
         }
-        return result;
+
+        for(auto it = mp.begin(); it != mp.end(); it++)
+        {
+            vector<int> vec;
+            for(auto it1 = it->second.begin(); it1 != it->second.end(); it1++)
+            {
+                vec.insert(vec.end(), it1->second.begin(), it1->second.end());
+            }
+            ans.push_back(vec);
+        }
+
+
+        return ans;
     }
 };
