45838ed8b7abdc82906a918bc0bd867800616729
Sourabh Chandel <77907942+sourabh59-coder@users.noreply.github.com>
2024-10-02 15:44:17 +0530

    Time: 10 ms (23.87%), Space: 20.9 MB (5.07%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
@@ -2,28 +2,34 @@ class Solution {
 public:
     int fun(vector<int> &v,int ind,int n,int buy,vector<vector<int>> &dp)
     {
-        if(ind==n)  return 0;
+        if(ind==n)
+        {
+            return 0;
+        }
 
         if(dp[ind][buy]!=-1)    return dp[ind][buy];
 
-        if(buy)
+        if(buy==1)
         {
-            int notTake = 0 + fun(v,ind+1,n,buy,dp);
-            int Take = -v[ind] + fun(v,ind+1,n,0,dp);
+            int take = v[ind] + fun(v,ind+1,n,0,dp);
 
-            return dp[ind][buy] = max(Take,notTake);
+            int notTake = 0 + fun(v,ind+1,n,buy,dp);
+
+            return dp[ind][buy] = max(take,notTake);
         }
         else
         {
-            int notTake = 0 + fun(v,ind+1,n,buy,dp);
-            int Take = v[ind] + fun(v,ind+1,n,1,dp);
+            int take = -v[ind] + fun(v,ind+1,n,1,dp);
 
-            return dp[ind][buy] = max(Take,notTake);
+            int notTake = 0 + fun(v,ind+1,n,buy,dp);
+
+            return dp[ind][buy] = max(take,notTake);
         }
     }
     int maxProfit(vector<int>& v) {
         int n = v.size();
+        int buy = 0;
         vector<vector<int>> dp(n+1, vector<int> (2,-1));
-        return fun(v,0,n,1,dp);
+        return fun(v,0,n,buy,dp);
     }
 };
